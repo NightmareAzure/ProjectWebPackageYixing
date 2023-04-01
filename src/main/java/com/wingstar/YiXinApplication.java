abc15be961f4db7c3fc25f2d2f53b1cd2ea5ef61
NightmareAzure <2899506072@qq.com>
2023-04-01 14:46:12 +0800

    Initial commit

---
+++ src/main/java/com/wingstar/YiXinApplication.java
@@ -0,0 +1,12 @@
+package com.wingstar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YiXinApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(YiXinApplication.class, args);
+    }
+
+}
